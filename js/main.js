a6cc2830b1bf8d7bfb77b3a1d205d75138cc437b
Rafiur Rahman <rrfoundary@gmail.com>
2021-04-20 17:08:02 +0600

    Completed responsive admin dashboard page

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+  const toggle = document.querySelector(".toggle");
+  const navigation = document.querySelector(".navigation");
+  const main = document.querySelector(".main");
+  toggle.classList.toggle("active");
+  navigation.classList.toggle("active");
+  main.classList.toggle("active");
+  console.log(toggle);
+}
